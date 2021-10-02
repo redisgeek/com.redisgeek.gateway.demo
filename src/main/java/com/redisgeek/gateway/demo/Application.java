17480782c7f656cbf50ee542d18032af5ad9aeb6
DaShaun <dashaun@redislabs.com>
2021-10-02 13:32:24 -0500

    rename main class

--- src/main/java/com/redisgeek/gateway/demo/Application.java
+++ src/main/java/com/redisgeek/gateway/demo/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoGatewayApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoGatewayApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
